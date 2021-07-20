025e0818d634a68fe064d336265f1da2cc49e997
hmk10 <halitmk@gmail.com>
2021-07-20 12:21:28 -0400

    GetNotPresentElementThreadSleep

---
+++ src/Day8/_08_GetNotPresentElementThreadSleep.java
@@ -0,0 +1,30 @@
+package Day8;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _08_GetNotPresentElementThreadSleep {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
+        WebElement startButton = driver.findElement(By.cssSelector("#start>button"));
+
+        startButton.click();
+
+        Thread.sleep(5000);
+
+        // no such element exception because element is not present in website yet
+        WebElement text = driver.findElement(By.id("finish"));
+        String textStr = text.getText();
+        System.out.println(textStr);
+
+    }
+
+}
