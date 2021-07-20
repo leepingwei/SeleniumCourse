91bba2eff85b3bc5a9fe59cae118e6f26f12a1a8
hmk10 <halitmk@gmail.com>
2021-07-20 12:21:10 -0400

    PageLoadTimeout

---
+++ src/Day8/_06_PageLoadTimeout.java
@@ -0,0 +1,22 @@
+package Day8;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class _06_PageLoadTimeout {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(4, TimeUnit.SECONDS);
+        // if the page is not loaded in 4 seconds you will get an error!!!
+        driver.get("http://the-internet.herokuapp.com/dynamic_controls");
+
+        driver.quit();
+
+    }
+
+}
